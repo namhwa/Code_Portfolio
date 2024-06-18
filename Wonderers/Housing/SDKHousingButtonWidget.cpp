d438af6ddec99ee55aa2f387cfacefb35a99a8ff
namhwa <45647091+namhwa@users.noreply.github.com>
2024-06-18 18:34:46 +0900

    Update SDKHousingButtonWidget.cpp

--- Wonderers/Housing/SDKHousingButtonWidget.cpp
+++ Wonderers/Housing/SDKHousingButtonWidget.cpp
@@ -15,8 +15,9 @@
 #include "PaperSprite.h"
 
 USDKHousingButtonWidget::USDKHousingButtonWidget()
+: bActive(true)
 {
-	bActive = true;
+	
 }
 
 void USDKHousingButtonWidget::SetHousingMenuIndex(int32 index)
@@ -26,20 +27,20 @@ void USDKHousingButtonWidget::SetHousingMenuIndex(int32 index)
 		return;
 
 	auto TextureTable = USDKTableManager::Get()->FindTableTextureSprite(GlobalTable->Value[index]);
-	if(TextureTable == nullptr || TextureTable->TexturePath.GetUniqueID().IsAsset() == false)
-		return;
-
-	SetImageTexturePath(ImageIcon, TextureTable->TexturePath.ToString());
+	if(TextureTable)
+	{
+		SetImageTexturePath(ImageIcon, TextureTable->TexturePath.ToString());
+	}
 
 	USDKWidgetParam* pParam = SDKCheckBoxMenu->GetClickedParam();
-	if(pParam == nullptr)
+	if(!IsValid(pParam))
 	{
 		CreateWidgetParam(index);
 	}
 	else
 	{
 		USDKWidgetParamInt32* pIntParam = Cast<USDKWidgetParamInt32>(pParam);
-		if(pIntParam != nullptr)
+		if(IsValid(pIntParam))
 		{
 			pIntParam->SetValue(index);
 		}
@@ -57,50 +58,56 @@ void USDKHousingButtonWidget::SetButtonRotation(bool bRotate)
 
 void USDKHousingButtonWidget::SetButtonSize(FVector2D vSize)
 {
-	if(SizeBoxButton == nullptr)
-		return;
-
-	SizeBoxButton->SetWidthOverride(vSize.X);
-	SizeBoxButton->SetHeightOverride(vSize.Y);
+	if(IsValid(SizeBoxButton))
+	{
+		SizeBoxButton->SetWidthOverride(vSize.X);
+		SizeBoxButton->SetHeightOverride(vSize.Y);
+	}
 }
 
 void USDKHousingButtonWidget::CreateWidgetParam(int32 value)
 {
 	USDKWidgetParamInt32* pNewParam = NewObject<USDKWidgetParamInt32>(this, USDKWidgetParamInt32::StaticClass());
-	if(pNewParam == nullptr)
-		return;
-
-	pNewParam->SetValue(value);
-	SDKCheckBoxMenu->SetClickedParam(pNewParam);
-	SDKCheckBoxMenu->OnCheckStateChangedParam.AddDynamic(this, &USDKHousingButtonWidget::OnClickedMenu);
+	if(IsValid(pNewParam))
+	{
+		pNewParam->SetValue(value);
+		SDKCheckBoxMenu->SetClickedParam(pNewParam);
+		SDKCheckBoxMenu->OnCheckStateChangedParam.AddDynamic(this, &USDKHousingButtonWidget::OnClickedMenu);
+	}
 }
 
 void USDKHousingButtonWidget::OnClickedMenu(bool bIsChecked, USDKWidgetParam* param)
 {
-	if(param == nullptr)
+	if(!IsValid(param))
+	{
 		return;
+	}
 
 	USDKWidgetParamInt32* pIntParam = Cast<USDKWidgetParamInt32>(param);
-	if(pIntParam == nullptr)
-		return;
-
-	ASDKHUD* SDKHUD = Cast<ASDKHUD>(GetOwningPlayer()->GetHUD());
-	if(SDKHUD == nullptr || SDKHUD->GetUI(EUI::Lobby_UIHousing) == nullptr)
-		return;
-
-	USDKHousingWidget* pHousingWidget = Cast<USDKHousingWidget>(SDKHUD->GetUI(EUI::Lobby_UIHousing));
-	if(pHousingWidget == nullptr)
-		return;
-
-	bActive = !bActive;
-
-	EHousingMenu menu = (EHousingMenu)pIntParam->GetValue();
-	switch(menu)
+	if(IsValid(pIntParam))
 	{
-	case EHousingMenu::HiddenList:	pHousingWidget->SetHiddenListState(bActive);			break;
-	case EHousingMenu::Camera:		pHousingWidget->ToggleRotatorHousingCamera(!bActive);	break;
-	case EHousingMenu::Save:		pHousingWidget->SaveHousingArrangeData();				break;
-	default:
-		break;
+		ASDKHUD* SDKHUD = Cast<ASDKHUD>(GetOwningPlayer()->GetHUD());
+		if(IsValid(SDKHUD))
+		{
+			USDKUserWidget* MainWidget = SDKHUD->GetUI(EUI::Lobby_UIHousing);
+			if(IsValid(MainWidget))
+			{
+				USDKHousingWidget* HousingWidget = Cast<USDKHousingWidget>(MainWidget);
+				if(IsValid(HousingWidget))
+				{
+					bActive = !bActive;
+				
+					EHousingMenu menu = (EHousingMenu)pIntParam->GetValue();
+					switch(menu)
+					{
+					case EHousingMenu::HiddenList:		HousingWidget->SetHiddenListState(bActive);		break;
+					case EHousingMenu::Camera:		HousingWidget->ToggleRotatorHousingCamera(!bActive);	break;
+					case EHousingMenu::Save:		HousingWidget->SaveHousingArrangeData();		break;
+					default:
+						break;
+					}
+				}
+			}
+		}
 	}
 }
